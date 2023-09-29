c134975820be444e8b03ab91828dffad065965d7
JosephHenderson <josephforcollege1@gmail.com>
2023-09-29 17:40:10 -0600

    pushing what's needed for supercomputer

--- speed_test.py
+++ speed_test.py
@@ -7,28 +7,35 @@ from collections import Counter
 import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+import pickle
 #import profile
 #import pstats
-from importlib import reload
 
 def create_berthas(top_power2=13):
     print("Creating Graphs")
     #sizes = list(set([int(2**i) for i in np.linspace(1,14,100)]))
     sizes = 2**np.arange(5,top_power2+1)
     prog = tqdm(sizes,total = len(sizes))
-    berthas = {}
+    with open("bertha_storage.pkl",'rb') as infile:
+        berthas = pickle.load(infile)
 
-    for i in prog:
-        
+    for n in prog:
+        if n in berthas.keys():
+            print(f"Already created for size {n}")
+            prog.update()
+            continue
         try:
-            G = graphs.GenBertha(i)
-            berthas[i] = G
+            print(f"Generating for size {n}")
+            G = graphs.GenBertha(n)
+            berthas[n] = G
         except:
-            print(f"failed for size {i}")
+            print(f"failed for size {n}")
         prog.update()
+    with open("bertha_storage.pkl","wb") as outfile:
+        pickle.dump(berthas,outfile)
     return berthas,sizes
 
-def DuelOfMethods(bertha,verbose=False):
+def DuelOfMethods(bertha,verbose=False,fake_parallel=False):
     """compares networkx's normal eigenvalue catching method to our method
     PARAMETERS:
         size (int): how big of graph you want to compare with
@@ -45,7 +52,7 @@ def DuelOfMethods(bertha,verbose=False):
     
     # our method
     start = pc()
-    our_spec = ep_utils.GetSpectrumFromLEPs(bertha,verbose=verbose)
+    our_spec = ep_utils.GetSpectrumFromLEPs(bertha,verbose=verbose,fake_parallel=fake_parallel)
     end = pc()
     our_time = end-start
 
@@ -55,12 +62,36 @@ def DuelOfMethods(bertha,verbose=False):
     
     return our_time, their_time, accurate
 
-def test_speed(bertha_dict,sizes):
+def test_speed(bertha_dict,sizes,fake_parallel=False):
+    # get times if already recorded for certain sizes
+    if 'times.pkl' in os.listdir():
+        print("loading previously calculated times...")
+        try:
+            with open('times.pkl','rb') as infile:
+                time_dict = pickle.load(infile)
+                our_times = time_dict['us']
+                their_times = time_dict['them']
+                sizes_to_skip = time_dict['sizes']
+                acc_count = [1.0]*len(our_times)
+        except:
+            our_times = []
+            their_times = []
+            sizes_to_skip = []
+            acc_count = [] 
+    else:
+        our_times = []
+        their_times = []
+        sizes_to_skip = []
+        acc_count = []
 
-    our_times = []
-    their_times = []
-    acc_count = []
-
+    # eliminate sizes we already have times for
+    print(f"We already have times recorded for sizes: {sizes_to_skip}")
+    sizes = list(set(sizes) - set(sizes_to_skip))
+    if sizes == []:
+        print("no times for new sizes requested.")
+        return our_times, their_times, acc_count
+    else:
+        print(f"Testing speed for n = {sizes}")
     progress_bar = tqdm(sizes,total=len(sizes))
 
     for size in progress_bar:
@@ -68,7 +99,7 @@ def test_speed(bertha_dict,sizes):
         us, them, peter, acc = 0, 0, 0, 0
         iters = 2
         for i in range(iters):
-            us_i, them_i, acc_i = DuelOfMethods(bertha_dict[size])
+            us_i, them_i, acc_i = DuelOfMethods(bertha_dict[size],fake_parallel=fake_parallel)
             us, them, acc = us+us_i, them+them_i, acc+acc_i
         us, them, acc = us/iters, them/iters, acc/iters
         our_times.append(us)
@@ -76,11 +107,21 @@ def test_speed(bertha_dict,sizes):
         acc_count.append(acc)
         progress_bar.update()
 
+    # save the new recorded times
+    with open('times.pkl','wb') as out:
+        time_dict = dict(zip(['us','them','sizes'],[our_times,their_times,sizes_to_skip+sizes]))
+        pickle.dump(time_dict,out)
+    # save times recorded
     return our_times, their_times, acc_count
 
 def plot_result(our_times,their_times,acc_count,sizes,fname="speed_comp.png"):
-    plt.loglog(list(sizes),our_times,'-',color='forestgreen',label="Our Method")
-    plt.loglog(list(sizes),their_times,'-',color='maroon',label='Naiive Method')
+    sizes = np.array(list(sizes))
+    naive = lambda x: x**(2.1) * (their_times[0]/(sizes[0]**(2.1)))
+    optimized = lambda x: x**(3/2) * (our_times[0]/(sizes[0]**(3/2)))
+    plt.loglog(sizes,our_times,'.-',color='forestgreen',label="Our Method Time")
+    plt.loglog(sizes,their_times,'.-',color='maroon',label='Naiive Method Time')
+    plt.loglog(sizes,optimized(sizes),'--',color='forestgreen',label="Theoretical Time (Ours)",alpha=.8)
+    plt.loglog(sizes,naive(sizes),'--',color='maroon',label='Theoretical Time (Naiive)',alpha=.8)
     #plt.loglog(sizes,p(size_list),'-',color='midnightblue',label='Polynomial Regression (squared)')
     #plt.loglog(sizes,p1(size_list),'-',color='chartreuse',label='Their Polynomial Regression (cubed)')
 
@@ -90,11 +131,20 @@ def plot_result(our_times,their_times,acc_count,sizes,fname="speed_comp.png"):
     plt.show()
 
 if __name__ == "__main__":
+    fake_parallel = False
+    try:
+        sys.argv[1]
+        print("Conducting fake parallelized Speed Test")
+        fake_parallel = True
+    except:
+        print("Conducting normal Speed Test")
+    # get parameters of test
     graph_fname = input("Input the name of the speed test file to be saved (default: 'speed_comp.png): ")
     if graph_fname == '': graph_fname = 'speed_comp.png'       
     top_power2 = input("Input the highest power of 2 to go to when generating Bertha's sizes (default: 13): ")
     if top_power2 == '': top_power2 = 13
     else: top_power2 = int(top_power2)
+    # conduct test
     berthas, sizes = create_berthas(top_power2)
     us,them,acc = test_speed(berthas,sizes)
-    plot_result(us,them,acc,sizes)
+    plot_result(us,them,acc,sizes,fname=graph_fname)
