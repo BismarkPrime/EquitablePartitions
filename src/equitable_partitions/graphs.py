48e79dc7360330de267db3d46e6b43745a0f3de1
jrhmc1 <josephforcollege1@gmail.com>
2026-05-08 08:44:22 -0600

    updated pyproject.toml and refactoring changes for module path added

--- src/equitable_partitions/graphs.py
+++ src/equitable_partitions/graphs.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 from time import perf_counter as pc
 import ep_utils
 from collections import Counter
-import slurm_helper as h
+import src.slurm_helper as h
 import pandas as pd
 import json
 
